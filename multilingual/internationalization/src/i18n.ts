f6a731501825cdc41a62dec660bf4b79a41fc894
AliAsgharGill <stylinalivlogs@gmail.com>
2024-06-26 11:53:33 +0500

    all languages getting translated

--- multilingual/internationalization/src/i18n.ts
+++ multilingual/internationalization/src/i18n.ts
@@ -1,14 +1,14 @@
-import {notFound} from 'next/navigation';
-import {getRequestConfig} from 'next-intl/server';
- 
+import { notFound } from "next/navigation";
+import { getRequestConfig } from "next-intl/server";
+
 // Can be imported from a shared config
-const locales = ['en', 'de'];
- 
-export default getRequestConfig(async ({locale}) => {
+const locales = ["en", "ar", "de", "es", "fr", "hi", "id", "it", "ur"];
+
+export default getRequestConfig(async ({ locale }) => {
   // Validate that the incoming `locale` parameter is valid
   if (!locales.includes(locale as any)) notFound();
- 
+
   return {
-    messages: (await import(`../messages/${locale}.json`)).default
+    messages: (await import(`../messages/${locale}.json`)).default,
   };
 });
--- multilingual/internationalization/src/middleware.ts
+++ multilingual/internationalization/src/middleware.ts
@@ -1,14 +1,14 @@
-import createMiddleware from 'next-intl/middleware';
- 
+import createMiddleware from "next-intl/middleware";
+
 export default createMiddleware({
   // A list of all locales that are supported
-  locales: ['en', 'de'],
- 
+  locales: ["en", "ar", "de", "es", "fr", "hi", "id", "it", "ur"],
+
   // Used when no locale matches
-  defaultLocale: 'en'
+  defaultLocale: "en",
 });
- 
+
 export const config = {
   // Match only internationalized pathnames
-  matcher: ['/', '/(de|en)/:path*']
+  matcher: ["/", "/(en|ar|de|es|fr|hi|id|it|ur)/:path*"],
 };
